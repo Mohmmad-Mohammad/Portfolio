a7eee061311c94b3af5dbf7fdf2a3c87c4ad0ff2
محمد محمد <softech.mohammadthedark333@gmail.com>
2022-07-30 16:04:07 +0300

    =1

---
+++ assets/js/m.js
@@ -0,0 +1,12 @@
+$('.container').animatedHeadline({
+    animationType: "rotate-1",
+    animationDelay: 2500,
+    barAnimationDelay: 3800,
+    barWaiting: 800,
+    lettersDelay: 50,
+    typeLettersDelay: 150,
+    selectionDuration: 500,
+    typeAnimationDelay: 1300,
+    revealDuration: 600,
+    revealAnimationDelay: 1500
+  });
